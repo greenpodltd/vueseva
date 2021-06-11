8f05ceebf46cd370647898c71473baed9bd673ac
Hieu Tran <hieutran.fu@gmail.com>
2021-06-11 22:15:43 +0700

    add template

--- packages/vueseva/bin/cli.js
+++ packages/vueseva/bin/cli.js
@@ -1,9 +1,23 @@
 #!/usr/bin/env node
+const path = require("path");
+const fs = require("fs-extra");
+const fg = require("fast-glob");
 
-function run() {
+const rootDir = path.resolve(__dirname, "..");
+const appDir = process.cwd();
+const appDestination = path.resolve(appDir, "src/.vueseva");
+const templateDir = path.resolve(rootDir, "src/template");
+async function run() {
   const argv = process.argv.slice(2);
+
   if (argv[0] === "dev") {
-    console.log("dev");
+    // Create output directory
+    if (!fs.existsSync(appDestination)) {
+      await fs.mkdirp(appDestination);
+    }
+
+    const entries = await fg(`${templateDir}/**/*.*`);
+    console.log(entries);
   }
 }
 
---
+++ packages/vueseva/src/template/index.js
@@ -0,0 +1,45 @@
+const fs = require("fs");
+const path = require("path");
+const { createServer: createViteServer } = require("vite");
+
+async function createServer() {
+  const fastify = require("fastify")({
+    logger: true,
+  });
+  await fastify.register(require("fastify-express"));
+
+  const vite = await createViteServer({
+    server: { middlewareMode: "ssr" },
+  });
+
+  fastify.use(vite.middlewares);
+
+  fastify.use("*", async (request, reply) => {
+    const url = "/";
+
+    try {
+      let template = fs.readFileSync(
+        path.resolve(__dirname, "index.html"),
+        "utf-8"
+      );
+      template = await vite.transformIndexHtml(url, template);
+      const { render } = await vite.ssrLoadModule("./entry-server.ts");
+      const [appHtml] = await render(url, {});
+      const html = template.replace(`<!--ssr-outlet-->`, appHtml);
+      reply.status(200).send(html);
+    } catch (e) {
+      vite.ssrFixStacktrace(e);
+      console.error(e);
+      reply.status(500).send(e.message);
+    }
+  });
+
+  try {
+    await fastify.listen(3000);
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+}
+
+createServer();
